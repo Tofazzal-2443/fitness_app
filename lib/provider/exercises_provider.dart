d626888153659b439aa384f4d60ddc2c13e7148e
Tofazzal-2443 <tofazzal15-2443@diu.edu.bd>
2023-05-04 20:01:43 +0600

    provider class used

--- lib/provider/exercises_provider.dart
+++ lib/provider/exercises_provider.dart
@@ -1,6 +1,15 @@
+import 'package:fitness_app/http/custom_http.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../models/exercises_model.dart';
+
 
 class ExercisesProvider extends ChangeNotifier{
+
+  ExercisesModel ?exercisesModel;
+  Future<ExercisesModel> getHomeData() async {
+    exercisesModel = await CustomHttpRequest.fetchHomeData();
+    return exercisesModel!;
+  }
 
 }
